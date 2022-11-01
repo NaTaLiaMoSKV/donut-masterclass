a5881bd8a4a5a01c6fec3f473c7e727b7fcf84ea
Natalia Moskvychova <natashamoskv@gmail.com>
2022-11-01 13:36:34 +0200

    update script for read-more links

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -2,6 +2,9 @@
 const hiddenText = document.querySelector('.section-text--hidden');
 const openLink = document.querySelector('.read-more-link--open');
 const closeLink = document.querySelector('.read-more-link--close');
+const programhiddenText = document.querySelector('.section-text--hidden2');
+const programopenLink = document.querySelector('.read-more-link--open2');
+const programcloseLink = document.querySelector('.read-more-link--close2');
 
 const toggleMenu = () => {
 const isMenuOpen =
@@ -15,8 +18,22 @@ const scrollLockMethod = !isMenuOpen
 bodyScrollLock[scrollLockMethod](document.body);
   };
 
+const toggleMenu2 = () => {
+const isMenuOpen =
+programopenLink.getAttribute('aria-expanded') === 'true' || false;
+programopenLink.setAttribute('aria-expanded', !isMenuOpen);
+programhiddenText.classList.toggle('is-open');
+
+const scrollLockMethod = !isMenuOpen
+  ? 'disableBodyScroll'
+  : 'enableBodyScroll';
+bodyScrollLock[scrollLockMethod](document.body);
+  };
+
 openLink.addEventListener('click', toggleMenu);
 closeLink.addEventListener('click', toggleMenu);
+programopenLink.addEventListener('click', toggleMenu2);
+programcloseLink.addEventListener('click', toggleMenu2);
 
 window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
 if (!e.matches) return;
